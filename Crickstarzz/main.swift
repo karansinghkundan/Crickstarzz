98323111da10c3be8486e1fcc3543056320acb29
karan <karansinghkundan@gmail.com>
2019-06-24 13:47:52 -0400

    Initial Commit

---
+++ Crickstarzz/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Crickstarzz
+//
+//  Created by MacStudent on 2019-06-24.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
